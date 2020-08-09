9281447ad32acd11698e38fb624cf69acebdb9ad
Dương Bảo Hưng <56499659+motcondicontrinh1@users.noreply.github.com>
2020-08-10 05:56:15 +0700

    custom App component to fit chakra-ui

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NextApp from "next/app";
+import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
+import { theme } from "../theme/theme";
+
+class App extends NextApp {
+  render() {
+    const { Component } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <ColorModeProvider>
+          <Component />
+        </ColorModeProvider>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
